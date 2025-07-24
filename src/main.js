62aedece0473d26bc5f9b72daf741852dbb210a3
orvice L <orvice@gmail.com>
2025-07-24 23:45:03 +0800

    Update import statement in main.js to use '@unhead/vue/client' for head management

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
-import { createHead } from '@unhead/vue'
+import { createHead } from '@unhead/vue/client'
 
 // Components
 import App from './App.vue'
